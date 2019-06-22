12f3ea243c14a5a8ae9805313c2822a1895ff9bf
alecpo <alessandro_vzp@hotmail.com>
2019-06-21 22:45:10 -0300

    erro no task.push() - undefined is not a funcion (evaluating tasks.push({...})

--- tasks/src/screens/Agenda.js
+++ tasks/src/screens/Agenda.js
@@ -14,6 +14,8 @@ import todayImage from '../../assets/imgs/today.jpg'
 import commonStyles from '../commonStyles'
 import Task from '../componentes/Task'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import ActionButton from 'react-native-action-button'
+import AddTask from './AddTask'
 
 export default class Agenda extends Component {
     state = {
@@ -45,8 +47,22 @@ export default class Agenda extends Component {
         ],
         visibleTasks: [],
         showDoneTasks: true,
+        showAddTask: false,
     }
     
+    addTask = task => {
+        const tasks = {...this.state.tasks}
+        tasks.push({
+            id: Math.random(),
+            desc: task.desc,
+            estimateAt: task.date,
+            doneAt: null
+        })
+
+        this.setState({ tasks, showAddTask: false }
+            , this.filterTasks)
+    }
+
     filterTasks = () => {
         let visibleTasks = null
         if(this.state.showDoneTasks) {
@@ -81,6 +97,9 @@ export default class Agenda extends Component {
     render() {
         return (
             <View style={styles.container}>
+                <AddTask isVisible={this.state.showAddTask}
+                    onSave={this.addTask}
+                    onCancel={() => this.setState({ showAddTask: false })} />
                 <ImageBackground source={todayImage} 
                     style={styles.background}>
                     <View style={styles.iconBar}>
@@ -102,6 +121,8 @@ export default class Agenda extends Component {
                         renderItem={({ item }) => 
                             <Task {...item} toggleTask={this.toggleTask} />} />
                 </View>
+                <ActionButton buttonColor={commonStyles.colors.today}
+                    onPress={() => { this.setState({ showAddTask: true}) }} />
             </View>
         )
     }
